15da4d26fe37dc0ed98182b187a0bfdbb3051c7a
hoaithanhsp <tolyhogroup@gmail.com>
2026-03-05 08:33:48 +0700

    Update accounts.ts

--- data/accounts.ts
+++ data/accounts.ts
@@ -2,6 +2,7 @@
 export const accounts = [
     { username: "admin", password: "admin123", name: "Quản trị viên" },
     { username: "duonghangdtntls@gmail.com", password: "SKKN100", name: "GV" },
+    { username: "trivvt@gmail.com", password: "SKKN100", name: "GV" },
     { username: "thuthuyntl26@gmail.com", password: "SKKN100", name: "GV" },
     { username: "luanvuthptnguyenvancu.edu.vn@gmail.com", password: "SKKN100", name: "GV" },
     { username: "Nguyenthanhtrung032018@gmail.com", password: "SKKN100", name: "GV" },
@@ -208,6 +209,7 @@ export const accounts = [
     { username: "Xhuong91@gmail.com", password: "SKKN100", name: "Nguyễn Thị Xuân Hương" },
     { username: "giaovien", password: "gv2025", name: "Giáo viên" },
 ];
+
 
 
 
